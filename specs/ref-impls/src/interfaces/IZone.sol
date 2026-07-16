1a341f6ed338f725d389f131f34ce0f9edb66e51
Tanishk Goyal <goyaltanishk02@gmail.com>
2026-07-16 17:43:58 +0530

    refactor: address sequencer portal review feedback

--- specs/ref-impls/src/interfaces/IZone.sol
+++ specs/ref-impls/src/interfaces/IZone.sol
@@ -642,7 +642,7 @@ interface IZonePortal {
     error InvalidBouncebackRecipient();
     error InvalidDepositTransition();
     error InvalidSequencerSet();
-    error SequencerSetUnchanged();
+    error SequencerConfigurationUnchanged();
     error InvalidQuorumCertificate();
     error LegacyBatchSubmissionDisabled();
 
