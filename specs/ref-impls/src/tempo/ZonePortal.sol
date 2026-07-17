eda877ac21d380c2e0c4e4ffaa773cecfa7c951a
0xalpharush <0xalpharush@protonmail.com>
2026-07-16 21:03:12 -0500

    test: cover malformed callback FIFO recovery

--- specs/ref-impls/src/tempo/ZonePortal.sol
+++ specs/ref-impls/src/tempo/ZonePortal.sol
@@ -843,7 +843,9 @@ contract ZonePortal is IZonePortal {
                 withdrawal.callbackData
             ) {
                 success = true;
-            } catch { }
+            } catch {
+                success = false;
+            }
         }
 
         if (!success) {
